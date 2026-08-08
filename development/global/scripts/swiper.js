c0226cd038708543c0579a33398afd8c5022c37e
Anton Shchukin <a.a.shchukin@gmail.com>
2026-08-08 19:28:36 +0700

    blog via swiper

--- development/global/scripts/swiper.js
+++ development/global/scripts/swiper.js
@@ -159,5 +159,39 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 
+    /* Слайдер "summary" для новинок и рекомендаций */
+
+    new Swiper('.swiper--blog', {
+        autoHeight: true,
+        spaceBetween: parseInt(containerPaddingStyle, 10) || 20,
+        navigation: {
+            prevEl: '.swiper-control--prev',
+            nextEl: '.swiper-control--next',
+            disabledClass: 'swiper-control--disabled',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+                slidesPerGroup: 1,
+                pagination: {
+                    type: 'fraction',
+                },
+            },
+            768: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+                pagination: {
+                    type: 'bullets',
+                    clickable: true,
+                },
+            }
+        }
+    });
+
+
 
 });
