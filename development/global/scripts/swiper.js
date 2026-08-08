e290a5ed885da22b332fb9d8495870f2278f82fc
Anton Shchukin <a.a.shchukin@gmail.com>
2026-08-08 17:36:25 +0700

    summary with carousels

--- development/global/scripts/swiper.js
+++ development/global/scripts/swiper.js
@@ -88,5 +88,39 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 
+    /* Слайдер "summary" для новинок и рекомендаций */
+
+    new Swiper('.swiper--summary', {
+        autoHeight: true,
+        spaceBetween: parseInt(containerPaddingStyle, 10) || 20,
+        navigation: {
+            prevEl: '.swiper-control--prev',
+            nextEl: '.swiper-control--next',
+            disabledClass: 'swiper-control--disabled',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+                slidesPerGroup: 1,
+                pagination: {
+                    type: 'fraction',
+                },
+            },
+            768: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+                pagination: {
+                    type: 'bullets',
+                    clickable: true,
+                },
+            }
+        }
+    });
+
+
 
 });
